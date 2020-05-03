18bc518f05cea55717378ba57fe5f2007de2cb2f
Demetrio <demetrioalmeida@gmail.com>
2020-05-03 19:11:55 -0400

    third commit

--- article.js
+++ article.js
@@ -1,31 +1,28 @@
 // API key
 var apiKey = "j35DYxgdTMaAnK6LR98HkviwSyGhfIul"
 
-var userSearch = "";
-
-// create a event listener for the search button
+// Event listener for the search button
 $("#search-button").on("click", function (event) {
     // event.preventDefault() function prevents the forms from trying to submit itself.
     event.preventDefault();
 
-    // grab the value form users input
-    // save the value to varible userSearch
-    var userSearch = $("#search").val().trim();
-
-    var itWorks = $("<h2>");
-    itWorks.attr("style", "background: yellow; font-family: Arial");
-    itWorks.attr("data-name", userSearch);
-    itWorks.text(userSearch);
-    $("#main-content").prepend(itWorks);
+    // call search function
+    searchArticle();
 })
 
 // access api in order to retrive articles
 // append the resolt on div #main-content
 
 
+function searchArticle() {
+    
+    // grab the value form users input
+        // save the value to varible userSearch
+    var userSearch = $("#search").val().trim();
+    var queryURL = "https://core.ac.uk:443/api-v2/search/" + userSearch + "?page=1&pageSize=10&apiKey=" + apiKey;
 
-
-var queryURL = "https:core.ac.uk:443/api-v2/search/" + userSearch + "?page=5&pageSize=10&apiKey=" + apiKey;
+    // clear input value
+    $("#search").val("");
 
     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
     $.ajax({
@@ -33,9 +30,26 @@ var queryURL = "https:core.ac.uk:443/api-v2/search/" + userSearch + "?page=5&pag
         method: "GET"
     }).then(function(response) {
         console.log(response);
+        var authors = response.data[0]._source.authors[0];
+        console.log(authors);
+        var description = response.data[0]._source.description;
+        var datePublished = response.data[0]._source.datePublished;
+        var topics = response.data[0]._source.topics[0];
+        var urls = response.data[0]._source.urls[0];
+        console.log(description);
+        console.log(datePublished);
+        console.log(topics);
+        console.log(urls);
+        
     });
 
+    var itWorks = $("<h2>");
+    itWorks.attr("style", "background: yellow; font-family: Arial");
+    itWorks.attr("data-name", userSearch);
+    itWorks.text("It works");
+    $("#main-content").prepend(itWorks);
 
+}
 /*
 var apiKey = "j35DYxgdTMaAnK6LR98HkviwSyGhfIul";
 
