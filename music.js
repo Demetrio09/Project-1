ba2ca852e0269c8efc996c72cbf128fd1333ab3b
Kim Le <kimle.lek@gmail.com>
2020-05-07 19:18:04 -0400

    auio componenets and playlist

--- music.js
+++ music.js
@@ -8,60 +8,92 @@ var playlist = $('#playlist');
 // var spiegels = new Audio("songs/Spiegel im Spiegel.mp3")
 // var sky = new Audio("songs/Written On The Sky.mp3")
 var songs = [
-  'songs/A quai.mp3',
   "songs/Moonlight Sonata.mp3",
+  "songs/A quai.mp3",
   "songs/Divenire.mp3",
-  'songs/Primavera.mp3',
+  "songs/Primavera.mp3",
   "songs/Ride of the Valkyries.mp3",
   "songs/Spiegel im Spiegel.mp3",
   "songs/Written On The Sky.mp3",
-]
-function createPlaylist(){
-  var list = $('<ul>')
-  for (var i = 0; i < songs.length; i++){
-    var item = $('<li>')
-    item.append(document.createTextNode(songs[i]))
-    list.append(item);
-  }
-  return list 
+];
+
+var songTittle = $('#songTittle');
+var song = new Audio();
+var currentSong = 0;
+// var fillBar = $('#fill');
+
+window.onload = playSong;
+// $('#play').on('click', playSong);
+
+function playSong(){
+  song.src = songs[currentSong];
+  songTittle.text(songs[currentSong]);
+  song.play();
 }
 
-$('#playlist').append(createPlaylist());
-$('#playlist').on('click', function (e){
-  console.log(e)
+function playOrPauseSong(){
+  if (song.paused){
+    song.play();
+    $("#play material-icons").attr("data-name", "play_circle_filled")
+  } else {
+    song.pause();
+    $("#play material-icons").attr("data-name", "pause_circle_filled")
+  }
+}
+
+song.addEventListener('timeupdate', function () {
+  // var position = song.currentTime / song.duration;
+  // fillBar.style.width = position*100+'%';
+  if(song.ended){
+    next();
+  }
 })
 
-// $.ajax({
-//   url: musicQuery,
-//   method: "GET"
-// }).then(function (response) {
-//   for (var i = 2; i < response.tracks.track.length; i = i + 7) {
-//       // console.log(musicQuery);
-//       // console.log(response);
-//     var songInfo = response.tracks.track[i]
-//     // console.log(songInfo);
-//     var songList = $('<ul>')
-//     var songTittle = songInfo.name
-//     // console.log(songTittle);
-//     $('<li>').text(songTittle);
-//     var artist = songInfo.artist.name
-//     $('<li>').text(artist);
-//     var ref = songInfo.url
-//     var link = $('<p>').text(ref)
-//     console.log(link);
-//     var songURL = $('<a>',{
-//       href : link,
-//       text : "Discover more from this artist"
-//     });
-//     songList.append(songTittle, artist, songURL)
-//     playlist.append(songList);
-//   }
+function next(){
+  currentSong ++;
+  if(currentSong > 6){
+    currentSong = 0;
+  }
+  playSong();
+  $("play material-icons").attr("data-name", "pause_circle_filled");
+}
 
-// });
-//create an song folder with all the mp3 files 
-//create ajax function to get information from API 
-  //get song tittle and artist and image 
-//append info to music div 
-//write function to play mp3 
-//write function to display only info related to the mp3 file that is playing 
-  //compare name? somehow tie the audio file to the info? 
+function pre(){
+  currentSong --;
+  if(currentSong < 0){
+    currentSong = 6;
+  }
+  playSong();
+  $("play material-icons").attr("data-name", "pause_circle_filled");
+}
+
+
+$.ajax({
+  url: musicQuery,
+  method: "GET"
+}).then(function (response) {
+  for (var i = 2; i < response.tracks.track.length; i = i + 7) {
+      // console.log(musicQuery);
+      console.log(response);
+    var songInfo = response.tracks.track[i]
+    // console.log(songInfo);
+    var songList = $('<ul>')
+    songList.addClass('inside')
+    var songTittle = songInfo.name
+    // console.log(songTittle);
+    $('<li>').text(songTittle);
+    var artist = songInfo.artist.name
+    $('<li>').text(artist);
+    var ref = songInfo.artist.url
+    var link = $('<p>').text(ref)
+    console.log(ref);
+    var songURL = $('<a>',{
+      href : link,
+      text : "Discover more from this artist"
+    });
+    songList.append(songTittle, artist, songURL)
+    playlist.append(songList);
+  } 
+
+});
+
